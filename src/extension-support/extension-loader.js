ce366926559766a4828cb317ad94325df6b5eefa
Yeqin Gong <gon@gyeq.in>
2023-07-03 16:33:44 +0800

    chore: refactor extensions loading

--- src/extension-support/extension-loader.js
+++ src/extension-support/extension-loader.js
@@ -1,13 +1,16 @@
 const AsyncLimiter = require('async-limiter');
+const Clone = require('../util/clone');
 const formatMessage = require('format-message');
 const ScratchCommon = require('./extension-api');
 const GUIComponent = require('./gui-component');
-const fetchExtensionData = require('../util/fetch-extension-data');
 const dispatch = require('../dispatch/central-dispatch');
 const {parseURL, validURL} = require('../util/url-util');
 
-const translationMap = {};
+const camel2under = str => str.replace(/([A-Z])/g, '_$1').toLowerCase();
+const url2id = str => str.replace(/[^a-z1-9_]/ig, '').toLowerCase();
 
+// setup global translations
+const translationMap = {};
 const setupTranslations = () => {
     const {
         locale,
@@ -19,220 +22,61 @@ const setupTranslations = () => {
     formatMessage.setup({translations});
 };
 
+// load remote script
 const loadScript = url => new Promise((resolve, reject) => {
+    const elem = document.querySelector(`#_${url2id(url)}`);
+    if (elem) {
+        return resolve();
+    }
     const script = document.createElement('script');
     script.onerror = () => {
         reject(new Error(`Error in script ${url}`));
     };
     script.onload = resolve;
+    script.id = `#_${url2id(url)}`;
     script.src = url;
     document.body.appendChild(script);
 });
 
+// load remote style
 const loadStyle = url => new Promise((resolve, reject) => {
+    const elem = document.querySelector(`#_${url2id(url)}`);
+    if (elem) {
+        return resolve();
+    }
     const link = document.createElement('link');
     link.rel = 'stylesheet';
     link.onerror = () => {
         reject(new Error(`Error in style ${url}`));
     };
     link.onload = resolve;
+    link.id = `#_${url2id(url)}`;
     link.href = url;
     document.body.appendChild(link);
 });
 
-/**
- * Sets up the global.Scratch API for an unsandboxed extension.
- * @param {string} extensionURL - unsandboxed extension url
- * @param {VirtualMachine} vm - VirtualMachine
- * @returns {Promise<object[]>} Resolves with a list of extension objects when Scratch.extensions.register is called.
- */
-const setupUnsandboxedExtensionAPI = (extensionURL, vm) => new Promise(resolve => {
-    // Create a new copy of global.Scratch for each extension
-    const Scratch = Object.assign({}, global.Scratch || {}, ScratchCommon);
-    Scratch.vm = vm;
-
-    const pendingModules = [];
-
-    const extensionObjects = [];
-
-    const state = {};
-    const interrupts = new Set();
-
-    Scratch.gui = {
-        addon (option) {
-            setTimeout(() => {
-                vm.emit('SETUP_ADDON', option);
-            }, 0);
-        },
-
-        get Component () {
-            return class extends GUIComponent {
-                static set initialState (newState) {
-                    Object.assign(state, newState);
-                }
-                constructor () {
-                    super();
-                    if (this.constructor.observedState) {
-                        interrupts.add([
-                            this.constructor.observedState,
-                            this.stateChangedCallback.bind(this)
-                        ]);
-                        Object.defineProperties(
-                            this._state,
-                            Object.fromEntries(
-                                this.constructor.observedState.map(name => [
-                                    name,
-                                    {get: () => state[name]}
-                                ])
-                            )
-                        );
-                    }
-                }
-                setState (name, newValue) {
-                    if (typeof name === 'object') {
-                        Object.entries(name).forEach(args => this.setState(...args));
-                        return;
-                    }
-                    if (this.constructor.observedState.includes(name)) {
-                        const oldValue = state[name];
-                        state[name] = newValue;
-                        interrupts.forEach(([observed, callback]) => {
-                            if (observed.includes(name)) {
-                                callback(name, oldValue, newValue);
-                            }
-                        });
-                    } else {
-                        super.setState(name, newValue);
-                    }
-                }
-            };
-        }
-    };
-
-    Scratch.extensions = {
-        /**
-         * register unsandboxed extension.
-         * @param {object} extensionObject - unsandboxed extension object
-         */
-        register (extensionObject) {
-            extensionObjects.push(extensionObject);
-            resolve(extensionObjects);
-        },
-
-        /**
-         * use/load unsandboxed extension by id.
-         * @param {string} extensionId - extension id
-         * @return {Promise} -
-         */
-        use (extensionId) {
-            return fetchExtensionData()
-                .then(extensionData => {
-                    const extension = extensionData.find(e => e.extensionId === extensionId);
-                    if (extension) {
-                        return vm.extensionManager.loadExtensionURL(extension.extensionURL);
-                    }
-                });
-        },
-
-        /**
-         * get extension service by id.
-         * @param {string} extensionId - extension id
-         * @returns {Promise} -
-         */
-        getService (extensionId) {
-            const serviceName = vm.extensionManager._loadedExtensions.get(extensionId);
-            if (!serviceName) {
-                vm.emit('EXTENSION_IMPORTING', true);
-                return this.use(extensionId)
-                    .then(() => this.getService(extensionId))
-                    .finally(() => {
-                        vm.emit('EXTENSION_IMPORTING', false);
-                    });
-            }
-            return dispatch.call.bind(dispatch, serviceName);
-        },
-
-        /**
-         * set translations.
-         * @param {array} translations - translation map
-         */
-        translations (translations) {
-            for (const key in translations) {
-                const value = translations[key];
-                translationMap[key] = Object.assign(translationMap[key] || {}, value);
-            }
-            setupTranslations();
-        },
-
-        /**
-         * extension configuration value
-         *
-         * @param {string} extensionId - extension id
-         * @return {any} the configuration value for the given extensionId or null if it does
-         * not exist
-         */
-        getConfig (extensionId) {
-            // synchronous localStorage on the same domain
-            const arrCookie = document.cookie.split('; ');
-            for (let i = 0; i < arrCookie.length; i++) {
-                const arr = arrCookie[i].split('=');
-                if (arr[0].includes('token_')) {
-                    localStorage.setItem(arr[0], decodeURIComponent(arr[1]));
-                }
-            }
-            return JSON.parse(localStorage.getItem(`token_${extensionId}`));
-        }
-    };
-
-    const resolveUrl = url => parseURL(url, extensionURL);
-
-    Scratch.require = (url, hasExport = true) => new Promise((resolveModule, reject) => {
-        if (validURL(url)) {
-            (/\.css$/i.test(url) ? loadStyle(url) : loadScript(url))
-                .then(resolveModule)
-                .catch(reject);
-            return;
-        }
-        const scriptURL = resolveUrl(url);
-        if (!scriptURL) {
-            reject(new Error(`Unable to parse ${url}`));
-            return;
-        }
-        if (/\.css$/i.test(url)) {
-            loadStyle(scriptURL)
-                .then(resolveModule)
-                .catch(reject);
-        } else {
-            const promise = loadScript(scriptURL).catch(reject);
-            if (hasExport) {
-                pendingModules.push(resolveModule);
+// load local script
+const AsyncFunction = (...args) => {
+    const code = args.pop();
+    return new Function(...args, `
+        return new Promise(___resolve___ => {
+            const promise = ${code};
+            if (promise instanceof Promise) {
+                promise.then(___resolve___);
             } else {
-                promise.then(resolveModule);
+                ___resolve___();
             }
-        }
-    });
-    Scratch.require.resolve = resolveUrl;
-
-    Scratch.export = module => {
-        if (pendingModules.length > 0) {
-            const resolveModule = pendingModules.pop();
-            resolveModule(module);
-        }
-    };
-
-    global.Scratch = Scratch;
-});
-
-/**
- * Disable the existing global.Scratch unsandboxed extension APIs.
- * This helps debug poorly designed extensions.
- */
-const teardownUnsandboxedExtensionAPI = () => {
-    // We can assume global.Scratch already exists.
-    global.Scratch.extensions.register = () => {
-        throw new Error('Too late to register new extensions.');
-    };
+        })
+    `);
 };
+const localScript = url => new Promise((resolve, reject) => {
+    fetch(url)
+        .then(res => res.text())
+        .then(text => {
+            resolve(AsyncFunction('Scratch', 'require', 'exports', text));
+        })
+        .catch(reject);
+});
 
 /**
  * Load an unsandboxed extension from an arbitrary URL. This is dangerous.
@@ -241,11 +85,163 @@ const teardownUnsandboxedExtensionAPI = () => {
  * @returns {Promise<object[]>} Resolves with a list of extension objects if the extension was loaded successfully.
  */
 const loadUnsandboxedExtension = (extensionURL, vm) => new Promise((resolve, reject) => {
-    setupUnsandboxedExtensionAPI(extensionURL, vm).then(resolve);
-    loadScript(extensionURL).catch(reject);
-}).then(objects => {
-    teardownUnsandboxedExtensionAPI();
-    return objects;
+    // Create a new copy of global.Scratch for each extension
+    const Scratch = Object.assign({}, ScratchCommon);
+    Scratch.vm = vm;
+
+    const state = {};
+    const interrupts = new Set();
+
+    class StateComponent extends GUIComponent {
+        static set initialState (newState) {
+            Object.assign(state, newState);
+        }
+        constructor () {
+            super();
+            if (this.constructor.observedState) {
+                interrupts.add([
+                    this.constructor.observedState,
+                    this.stateChangedCallback.bind(this)
+                ]);
+                Object.defineProperties(
+                    this._state,
+                    Object.fromEntries(
+                        this.constructor.observedState.map(name => [
+                            name,
+                            {get: () => state[name]}
+                        ])
+                    )
+                );
+            }
+        }
+        setState (name, newValue, oldValue) {
+            if (typeof name === 'object') {
+                const oldState = Clone.simple(state);
+                Object.assign(state, name);
+                Object.entries(name).forEach(([k, v]) => this.setState(k, v, oldState[k]));
+                return;
+            }
+            if (this.constructor.observedState.includes(name)) {
+                oldValue = (typeof oldValue === 'undefined') ? state[name] : oldValue;
+                state[name] = newValue;
+                interrupts.forEach(([observed, callback]) => {
+                    if (observed.includes(name)) {
+                        callback(name, oldValue, newValue);
+                    }
+                });
+            } else {
+                super.setState(name, newValue);
+            }
+        }
+    }
+
+    Scratch.gui = {};
+    Scratch.gui.addon = option => setTimeout(() => vm.emit('SETUP_ADDON', option), 0);
+    Scratch.gui.Component = StateComponent;
+
+    const resolveUrl = url => parseURL(url, extensionURL);
+
+    const extensionObjects = [];
+
+    Scratch.extensions = {};
+    // register unsandboxed extension
+    Scratch.extensions.register = extensionObject => {
+        extensionObjects.push(extensionObject);
+        resolve(extensionObjects);
+    };
+    // use/load unsandboxed extension by id
+    Scratch.extensions.use = extensionId => {
+        if (vm.extensionManager.isExtensionLoaded(extensionId)) {
+            return Promise.resolve();
+        }
+        const url = resolveUrl(`../${camel2under(extensionId)}/index.js`);
+        return vm.extensionManager.loadExtensionURL(url);
+    };
+    // get extension service by id.
+    Scratch.extensions.getService = extensionId => {
+        const serviceName = vm.extensionManager._loadedExtensions.get(extensionId);
+        if (serviceName) {
+            return dispatch.call.bind(dispatch, serviceName);
+        }
+        vm.emit('EXTENSION_IMPORTING', true);
+        return Scratch.extensions.use(extensionId)
+            .then(() => Scratch.extensions.getService(extensionId))
+            .finally(() => {
+                vm.emit('EXTENSION_IMPORTING', false);
+            });
+    };
+    // setup translations
+    Scratch.extensions.translations = translations => {
+        for (const key in translations) {
+            const value = translations[key];
+            translationMap[key] = Object.assign(translationMap[key] || {}, value);
+        }
+        setupTranslations();
+    };
+    // extension configuration value from extension document domain
+    Scratch.extensions.getConfig = extensionId => {
+        // synchronous localStorage on the same domain
+        const arrCookie = document.cookie.split('; ');
+        for (let i = 0; i < arrCookie.length; i++) {
+            const arr = arrCookie[i].split('=');
+            if (arr[0].includes('token_')) {
+                localStorage.setItem(arr[0], decodeURIComponent(arr[1]));
+            }
+        }
+        return JSON.parse(localStorage.getItem(`token_${extensionId}`));
+    };
+
+    const exportsProxy = resolveModule => {
+        const exportsModule = {};
+        return new Proxy(new Function(), {
+            apply (target, thisArg, args) {
+                resolveModule(args[0]);
+            },
+            set (obj, prop, value) {
+                exportsModule[prop] = value;
+                resolveModule(exportsModule);
+            }
+        });
+    };
+
+    const requireModule = url => new Promise((resolveModule, rejectModule) => {
+        // load remote script or style
+        if (validURL(url)) {
+            (/\.css$/i.test(url) ? loadStyle(url) : loadScript(url))
+                .then(resolveModule)
+                .catch(rejectModule);
+            return;
+        }
+        // load local script or style
+        const scriptURL = resolveUrl(url);
+        if (!scriptURL) {
+            rejectModule(new Error(`Unable to parse ${url}`));
+            return;
+        }
+        if (/\.css$/i.test(url)) {
+            loadStyle(scriptURL)
+                .then(resolveModule)
+                .catch(rejectModule);
+        } else if (/\.json$/i.test(url)) {
+            fetch(scriptURL)
+                .then(res => res.json())
+                .then(resolveModule)
+                .catch(rejectModule);
+        } else {
+            const exportsModule = exportsProxy(resolveModule);
+            localScript(scriptURL)
+                .then(callScript => {
+                    callScript(Scratch, requireModule, exportsModule)
+                        .then(resolveModule);
+                })
+                .catch(rejectModule);
+        }
+    });
+    requireModule.resolve = resolveUrl;
+
+    localScript(extensionURL)
+        .then(callScript => callScript(Scratch, requireModule))
+        .catch(reject);
 });
 
 // Because loading unsandboxed extensions requires messing with global state (global.Scratch),
